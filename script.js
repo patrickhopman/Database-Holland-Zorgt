a8b06eae08ba94dd5db84975da984d4d13b93201
Patrick Hopman <patrickhopman@me.com>
2018-02-13 13:50:49 +0100

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+}
