ec2be3d78e84a0c4b09497f2d0164a673b5c093d
Patrick Hopman <patrickhopman@me.com>
2018-02-13 15:11:36 +0100

    Bootstrap

--- script.js
+++ script.js
@@ -2,14 +2,7 @@ window.onload=function(){
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
-  }
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+    message: 'Hallo! Vue.js werkt!'
   }
 })
 
